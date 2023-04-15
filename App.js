0424016dc6506bc8a82fc2de04b8396a01ae2e37
crescenthw <tjguddn9899@naver.com>
2023-04-15 21:23:58 +0900

    메인화면 정리

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is a title</Text>
       <StatusBar style="auto" />
+      <View style={{ flex: 1 }}>
+        <View style={styles.statusView}>
+          <Text>New Chat</Text>
+        </View>
+        <View style={styles.mainView}>
+          <Text>GPT API</Text>
+        </View>
+        <View style={styles.inputBar}>
+          <TextInput placeholder={"내용을 입력하세요"} style={styles.input} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +23,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
+    paddingVertical: 25,
+  },
+  statusView: {
+    flex: 1,
+    backgroundColor: "#74aa9c",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    alignItems: "center",
+  },
+  mainView: {
+    flex: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputBar: {
+    flex: 2,
+    width: "100%",
     alignItems: "center",
     justifyContent: "center",
+    borderTopWidth: 1,
+    borderColor: "grey",
+    borderOpacity: 0.8,
+  },
+  input: {
+    height: 50,
+    width: "95%",
+    borderColor: "#5a67ea",
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 10,
   },
 });
